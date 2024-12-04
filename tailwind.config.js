6173ecfbf54c8ef2f9667e5a48f93a94bb798992
Jason Norwood-Young <jason@10layer.com>
2024-12-04 12:24:29 +0100

    Redesign using Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+        './_includes/**/*.{html,md}',
+        './_layouts/**/*.{html,md}',
+        './_posts/**/*.{html,md}',
+        './*.{html,md}'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+} 
